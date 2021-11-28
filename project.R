cec998baa3ecf0a05fdb480e95955c32919b8446
Chris Lee <chreestopherlee@gmail.com>
2021-11-27 23:42:20 -0500

    Uploaded the original dataset, then uploaded a scraped dataset that contains team stats since 2006. :)  Then figured out which qb for each team has the most starts.  I don't really care about consecutive starts, more total starts

---
+++ project.R
@@ -0,0 +1,30 @@
+library(dplyr)
+library(tidyverse)
+library(sf)
+library(jsonlite)
+library(USAboundaries)
+library(leaflet)
+library(tibbletime)
+library(ggthemes)
+library(scales)
+library(ggplot2)
+
+httpgd::hgd()
+httpgd::hgd_browse()
+
+starts <- read.csv("data/Starts.csv")
+team <- read.csv("data/TeamStats.csv")
+
+glimpse(starts)
+
+abbr <- starts %>% group_by(Team) %>%
+    summarise(count = n())
+
+view(abbr)
+
+topqb <- starts %>% group_by(Team, Player) %>%
+    count(Player, name="TotalStarts") %>%
+    group_by(Team) %>%
+    slice(which.max(TotalStarts))
+
+topqb
